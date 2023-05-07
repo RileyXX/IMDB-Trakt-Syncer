e855093542ee8fec8d3b8115dc1c967114724d39
RileyXX <49823202+RileyXX@users.noreply.github.com>
2023-05-06 22:08:40 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.8'
-DESCRIPTION = 'This script will sync user ratings for Movies and TV Shows both ways between Trakt and IMDb.'
+VERSION = '1.0.9'
+DESCRIPTION = 'This python script will sync user ratings for Movies and TV Shows both ways between Trakt and IMDb.'
 
 # Setting up
 setup(
