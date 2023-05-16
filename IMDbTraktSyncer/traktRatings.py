8ef3e4e45b131808461a07c0712b6b5f602d6a69
RileyXX <49823202+RileyXX@users.noreply.github.com>
2023-05-15 20:39:39 -0500

    Add files via upload

--- IMDbTraktSyncer/traktRatings.py
+++ IMDbTraktSyncer/traktRatings.py
@@ -4,7 +4,7 @@ import subprocess
 import requests
 
 def getTraktRatings(trakt_client_id, trakt_access_token):
-    #Get Trakt Ratings
+    # Get Trakt Ratings
     print('Getting Trakt Ratings')
 
     headers = {
@@ -22,6 +22,7 @@ def getTraktRatings(trakt_client_id, trakt_access_token):
 
     movie_ratings = []
     show_ratings = []
+    episode_ratings = []
 
     for item in json_data:
         if item['type'] == 'movie':
@@ -32,8 +33,15 @@ def getTraktRatings(trakt_client_id, trakt_access_token):
             show = item['show']
             show_id = show['ids']['imdb']
             show_ratings.append({'Title': show['title'], 'Year': show['year'], 'Rating': item['rating'], 'ID': show_id, 'Type': 'show'})
+        elif item['type'] == 'episode':
+            show = item['show']
+            show_title = show['title']
+            episode = item['episode']
+            episode_id = episode['ids']['imdb']
+            episode_title = f'{show_title}: {episode["title"]}'
+            episode_ratings.append({'Title': episode_title, 'Year': episode.get('year'), 'Rating': item['rating'], 'ID': episode_id, 'Type': 'episode'})
 
-    trakt_ratings = movie_ratings + show_ratings
+    trakt_ratings = movie_ratings + show_ratings + episode_ratings
 
     print('Getting Trakt Ratings Complete')
     
