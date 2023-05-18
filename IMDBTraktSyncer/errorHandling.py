b860c52c3718e89a2f584426f87adc15d9075681
RileyXX <49823202+RileyXX@users.noreply.github.com>
2023-05-18 16:39:47 -0500

    Add files via upload

---
+++ IMDBTraktSyncer/errorHandling.py
@@ -0,0 +1,14 @@
+import traceback
+
+def report_error(error_message):
+    github_issue_url = "https://github.com/RileyXX/IMDB-Trakt-Syncer/issues/new"
+    traceback_info = traceback.format_exc()
+
+    print("\n--- ERROR ---")
+    print(error_message)
+    print("Please submit the error to GitHub with the following information:")
+    print("-" * 50)
+    print(traceback_info)
+    print("-" * 50)
+    print(f"Submit the error here: {github_issue_url}")
+    print("-" * 50)
