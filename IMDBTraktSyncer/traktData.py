49384cb4b5d3f71100f942d3a004ba4fcdb626e3
RileyXX <49823202+RileyXX@users.noreply.github.com>
2023-05-19 21:10:27 -0500

    Add files via upload

---
+++ IMDBTraktSyncer/traktData.py
@@ -0,0 +1,101 @@
+import json
+import requests
+import time
+
+def getTraktData(trakt_client_id, trakt_access_token):
+    # Process Trakt Ratings and Comments
+    print('Processing Trakt Ratings and Comments')
+
+    headers = {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': trakt_client_id,
+        'Authorization': f'Bearer {trakt_access_token}'
+    }
+
+    time.sleep(1) # avoid trakt rate limit
+    response = requests.get('https://api.trakt.tv/users/me', headers=headers)
+    json_data = json.loads(response.text)
+    username = json_data['username']
+
+    # Get Trakt Ratings
+    time.sleep(1) # avoid trakt rate limit
+    response = requests.get(f'https://api.trakt.tv/users/{username}/ratings', headers=headers)
+    json_data = json.loads(response.text)
+
+    movie_ratings = []
+    show_ratings = []
+    episode_ratings = []
+
+    for item in json_data:
+        if item['type'] == 'movie':
+            movie = item['movie']
+            movie_id = movie['ids']['imdb']
+            movie_ratings.append({'Title': movie['title'], 'Year': movie['year'], 'Rating': item['rating'], 'ID': movie_id, 'Type': 'movie'})
+        elif item['type'] == 'show':
+            show = item['show']
+            show_id = show['ids']['imdb']
+            show_ratings.append({'Title': show['title'], 'Year': show['year'], 'Rating': item['rating'], 'ID': show_id, 'Type': 'show'})
+        elif item['type'] == 'episode':
+            show = item['show']
+            show_title = show['title']
+            episode = item['episode']
+            episode_id = episode['ids']['imdb']
+            episode_title = f'{show_title}: {episode["title"]}'
+            episode_ratings.append({'Title': episode_title, 'Year': episode.get('year'), 'Rating': item['rating'], 'ID': episode_id, 'Type': 'episode'})
+
+    trakt_ratings = movie_ratings + show_ratings + episode_ratings
+
+    # Get Trakt Comments
+    time.sleep(1) # avoid trakt rate limit
+    response = requests.get(f'https://api.trakt.tv/users/{username}/comments', headers=headers)
+    json_data = json.loads(response.text)
+    total_pages = response.headers.get('X-Pagination-Page-Count')
+    trakt_comments = []
+
+    for page in range(1, int(total_pages) + 1):
+        time.sleep(1) # avoid trakt rate limit
+        response = requests.get(f'https://api.trakt.tv/users/{username}/comments', headers=headers, params={'page': page})
+        json_data = json.loads(response.text)
+
+        for comment in json_data:
+            comment_type = comment['type']
+            spoiler = comment.get('spoiler', False)
+            if comment_type == 'movie':
+                show_movie_or_episode_title = comment['movie']['title']
+                show_movie_or_episode_year = comment['movie'].get('year')
+                show_movie_or_episode_imdb_id = comment['movie']['ids']['imdb']
+            elif comment_type == 'episode':
+                show_movie_or_episode_title = f"{comment['show']['title']}: {comment['episode']['title']}"
+                show_movie_or_episode_year = comment['show'].get('year')
+                show_movie_or_episode_imdb_id = comment['episode']['ids']['imdb']
+            elif comment_type == 'show':
+                show_movie_or_episode_title = comment['show']['title']
+                show_movie_or_episode_year = comment['show'].get('year')
+                show_movie_or_episode_imdb_id = comment['show']['ids']['imdb']
+            elif comment_type == 'season':
+                show_movie_or_episode_title = f"{comment['show']['title']}: Season {comment['season']['number']}"
+                show_movie_or_episode_year = None
+                show_movie_or_episode_imdb_id = None
+            else:
+                show_movie_or_episode_title = None
+                show_movie_or_episode_year = None
+                show_movie_or_episode_imdb_id = None
+
+            comment_info = comment['comment']
+            trakt_comment_id = comment_info.get('id')
+            trakt_comment = comment_info.get('comment')
+
+            trakt_comments.append({
+                'Title': show_movie_or_episode_title,
+                'Year': show_movie_or_episode_year,
+                'ID': show_movie_or_episode_imdb_id,
+                'TraktCommentID': trakt_comment_id,
+                'Comment': trakt_comment,
+                'Spoiler': spoiler,
+                'Type': comment_type
+            })
+
+    print('Processing Trakt Ratings and Comments Complete')
+
+    return trakt_ratings, trakt_comments
--- IMDBTraktSyncer/verifyCredentials.py
+++ IMDBTraktSyncer/verifyCredentials.py
@@ -1,5 +1,6 @@
 import os
 import json
+import datetime
 try:
     from IMDBTraktSyncer import authTrakt
 except:
@@ -57,6 +58,83 @@ else:
     values["trakt_access_token"] = trakt_access_token
     with open(file_path, "w") as f:
         json.dump(values, f)
+
+# Last run function, used to determine when the last time IMDB reviews were submitted    
+def check_imdb_reviews_last_submitted():
+    # Load credentials from credentials.txt
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            credentials = json.load(file)
+    else:
+        credentials = {}
+
+    imdb_reviews_last_submitted_date_str = credentials.get('imdb_reviews_last_submitted_date')
+
+    # If imdb_reviews_last_submitted_date is not available or not in the correct format, consider it as 7 days ago
+    imdb_reviews_last_submitted_date = datetime.datetime.strptime(imdb_reviews_last_submitted_date_str, '%Y-%m-%d %H:%M:%S') if imdb_reviews_last_submitted_date_str else datetime.datetime.now() - datetime.timedelta(hours=168)
+
+    # Check if 168 hours have passed since the last run
+    if datetime.datetime.now() - imdb_reviews_last_submitted_date >= datetime.timedelta(hours=168):
+        # Update the imdb_reviews_last_submitted_date with the current time
+        credentials['imdb_reviews_last_submitted_date'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        # Save updated credentials to credentials.txt
+        with open(file_path, 'w') as file:
+            json.dump(credentials, file)
+
+        return True
+    else:
+        return False
+        
+def prompt_sync_reviews():
+    # Define the file path
+    here = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(here, 'credentials.txt')
+
+    # Check if credentials file exists
+    try:
+        with open(file_path, 'r') as file:
+            credentials = json.load(file)
+            sync_reviews_value = credentials.get('sync_reviews')
+            if sync_reviews_value is not None:
+                return sync_reviews_value
+    except FileNotFoundError:
+        pass
+
+    while True:
+        # Prompt the user for input
+        print("Ratings are synced by default. Please note: comments synced to IMDB will use \"My Review\" as the title field.")
+        print("Do you want to sync comments? (y/n)")
+        user_input = input("Enter your choice: ")
+
+        # Validate user input
+        if user_input.lower() == 'y':
+            sync_reviews_value = True
+            break
+        elif user_input.lower() == 'n':
+            sync_reviews_value = False
+            break
+        else:
+            # Invalid input, ask again
+            print("Invalid input. Please enter 'y' or 'n'.")
+
+    # Update the value in the JSON file
+    credentials = {}
+    try:
+        with open(file_path, 'r') as file:
+            credentials = json.load(file)
+    except FileNotFoundError:
+        pass
+
+    credentials['sync_reviews'] = sync_reviews_value
+
+    with open(file_path, 'w') as file:
+        json.dump(credentials, file)
+
+    # return true or false
+    return sync_reviews_value
+sync_reviews_value = prompt_sync_reviews()
+
 
 # Save the credential values as variables
 trakt_client_id = values["trakt_client_id"]
