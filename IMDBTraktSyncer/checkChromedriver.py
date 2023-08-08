b1c607570381e47fee72938194274d11e752e63a
RileyXX <49823202+RileyXX@users.noreply.github.com>
2023-08-08 15:50:15 -0500

    Add files via upload

--- IMDBTraktSyncer/checkChromedriver.py
+++ IMDBTraktSyncer/checkChromedriver.py
@@ -50,7 +50,7 @@ def install_chromedriver_fallback_method():
     print("Using install chromedriver-py fallback method")
     # Install the chromedriver-py package using the Python interpreter
     feed = feedparser.parse('https://pypi.org/rss/project/chromedriver-py/releases.xml')
-    # Get the second latest release version
+    # Get the latest release version
     version = feed.entries[0].title.split()[-1]
     # Install the chromedriver-py package using pip
     subprocess.run([sys.executable, '-m', 'pip', 'install', '--no-cache-dir', f'chromedriver-py=={version}'])
